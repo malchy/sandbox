8c239393934187baffb12de6151f08cce3843f93
malt <gf_malch@yahoo.co.jp>
2014-01-25 20:16:22 +0900

    #4
    #4 変数と定数 まで完了

---
+++ ruby/dotinstall/hello.rb
@@ -0,0 +1,11 @@
+print "Hello world\n"             # そのまま表示
+print "Hello world", "world"      # 複数の文字列を続けて
+puts "hello world again"          # 改行で表示
+p "Hello world 3"                 #オブジェクト型がわかるように表示
+
+# コメント
+
+=begin
+
+ここはコメント
+=end
